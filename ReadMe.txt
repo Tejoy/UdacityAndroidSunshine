f96d1485542e0028592a1ef55e7eb9d6aee6ea62
Tejomai <tejomai@amazon.com>
2017-01-14 11:37:20 +0530

    Sunshine app upto upate data from remote url on refresh

---
+++ ReadMe.txt
@@ -0,0 +1,2 @@
+This is a project based on learnings from Udacity Nanodegree course of Android.
+It is an app names Sunshine to view the weather forecasts for next seven days based on the location.
---
+++ Sunshine/app/src/main/java/com/nanodegree/tejomai/sunshine/SunshineUtil.java
@@ -0,0 +1,10 @@
+package com.nanodegree.tejomai.sunshine;
+
+/**
+ * Created by tejomai on 06/01/17.
+ */
+
+public class SunshineUtil {
+
+    public static final String INTENT_EXTRA_DETAIL = "detail";
+}
